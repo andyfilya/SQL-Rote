7149fbf920398ca89fa9dbe51cfe50c0f7966255
Andrey Filkin <liamo19@mail.ru>
2024-03-09 14:18:52 +0300

    create main

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/sqlroute/internal/db"
-
+	"github.com/sqlroute/internal/handlers"
+	"github.com/sqlroute/internal/server"
+  "github.com/sqlroute/internal/route"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	mysqldb, err := db.InitDB()
+	db, err := db.InitDB()
 	if err != nil {
 		logrus.Errorf("error while init db in main function %v", err)
 		return
 	}
-	err = mysqldb.Db.Ping()
+	err = db.Ping()
 	if err != nil {
 		logrus.Errorf("error while first time ping new mysql database %v", err)
 		return
 	}
+  handlers := &handlers.Handler{
+    DB: db,
+  }
+  routes := route.InitRoutes(handlers)
+	err = server.StartWebServer("8081", routes)
+	if err != nil {
+		logrus.Errorf("server ended with error in main function %v", err)
+		return
+  }
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jstemmer/gotags v1.4.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
